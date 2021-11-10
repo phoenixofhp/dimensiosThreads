e620bb2ee0e9a9fa760f1e54726230110de24d75
phoenixofhp <64036649+phoenixofhp@users.noreply.github.com>
2021-11-11 01:22:52 +0300

    Add files via upload

--- dimensiosThreads/Source.cpp
+++ dimensiosThreads/Source.cpp
@@ -5,25 +5,24 @@
 #include<thread>
 #include<mutex>
 
-#define THREAD_LOOP
-//#define DEBUG_ON
-std::mutex mutex;
+#define THREAD_LOOP //Всегда включена в актуальной версии
+//#define DEBUG_ON //Вывод в консоль информации о том, сколько % точек уже найдено. Сильно замедляет работу, если размерность пространства <20-25
+std::mutex mutex; //Объяслен в глобальной области, правильно ли так делать?
 
-constexpr int THREADS = 2;
-constexpr int DIMENSIONS = 15;
-constexpr int AMOUNT = 10000;
-constexpr double RADIUS = 1.0;
-constexpr double EPSILON = 0.1;
+constexpr int THREADS = 4; //Кол-во потоков
+constexpr int DIMENSIONS = 15; //Размерность пространства
+constexpr int AMOUNT = 10000; //Кол-во точек, которых мы генерируем
+constexpr double RADIUS = 1.0; //Радиус сферы
+constexpr double EPSILON = 0.1; //Толщина внешнего слоя сферы
 constexpr double RADIUS_SQR = RADIUS * RADIUS;
 constexpr double RADIUS_EPSILON_SQR = (RADIUS - EPSILON) * (RADIUS - EPSILON);
-
 
 double rng(int seed) {
 
 	static std::default_random_engine rng(seed);
-	std::uniform_real_distribution<double> dist(-1.0, 1.0);
+	std::uniform_real_distribution<double> dist(-1.0 * RADIUS, 1.0 * RADIUS);
 	return dist(rng);
-}
+} //Возвращает случайную точку из квадрата со стороной RADIUS
 
 bool isSphere(std::vector<double>& point, int n) {
 
@@ -34,7 +33,7 @@ bool isSphere(std::vector<double>& point, int n) {
 	}
 
 	return true;
-}
+} //Проверяет, лежит ли точка внутри сферы
 
 bool isInLesserSphere(std::vector<double>& point) {
 
@@ -45,36 +44,49 @@ bool isInLesserSphere(std::vector<double>& point) {
 	}
 
 	return true;
-}
+} //Проверяет, лежит ли точка во внутреннем слое сферы
 
-void run( std::vector<std::vector<double>>* points,int seed) {
-	std::vector<double> point(DIMENSIONS, 0);
+void run(std::vector<std::vector<double>>* points,int seed) {
+
 	while (points->size() < AMOUNT) {
-		
-			
-			for (int j = 0; j < DIMENSIONS; j++) {
-				point[j] = rng(seed);
-				if (isSphere(point, j)) continue;
-				else j--;
-			}
-			mutex.lock();
-			points->push_back(point);
+		std::vector<std::vector<double>> point_n;
+		int j = -1;
 
-#ifdef DEBUG_ON
-			std::cout << 100.0 * (double)points->size() / (double)AMOUNT << "% точек найдено" << std::endl;
-#endif 
-			mutex.unlock();
-	
+		do 
+		{	
+			j++;
+			point_n.push_back(std::vector<double>(DIMENSIONS, 0)); //Инициализируем точку в пространстве нулями
+			for (int i = 0; i < DIMENSIONS; i++)
+			{
+				(point_n[j])[i] = (rng(seed));
+				if (isSphere(point_n[j], i)) continue;
+				else i--;
+			}
+		} 
+		while (!mutex.try_lock());
+
+		for (size_t i = 0; i < point_n.size(); i++)
+		{
+			points->push_back(point_n[i]);
+		}
+
+		#ifdef DEBUG_ON
+		std::cout << 100.0 * (double)points->size() / (double)AMOUNT << "% точек найдено" << std::endl;
+		#endif 
+
+		mutex.unlock();
 	}
 }
 
 int main() {
-	auto start = std::chrono::high_resolution_clock::now();
+
+	auto start = std::chrono::high_resolution_clock::now(); //Измеряю время работы программы при помощи методов из <chrono>
+
 	setlocale(LC_CTYPE, "rus");
 
 	std::thread threads[THREADS];
 
-	std::vector<std::vector<double>>* points;
+	std::vector<std::vector<double>>* points; //Вектор точек в куче, так как у него может быть очень большой объем
 	points = new std::vector<std::vector<double>>;
 	points->reserve(AMOUNT);
 
@@ -90,7 +102,8 @@ int main() {
 		std::thread th2(run, points,1);
 		std::thread th3(run, points,2);
 		std::thread th4(run, points,3);
-	#endif 
+	#endif
+
 	do
 	{
 		bool end = false;
@@ -122,11 +135,12 @@ int main() {
 				}
 			}
 		#endif
-	} while (false);
+
+	} while (false); //Одноразовый цикл, созданный для того, чтобы сделать переменную end локальной для данного блока кода
 
 	int outside = points->size();
-	for (size_t i = 0; i < points->size(); i++) {
-
+	for (size_t i = 0; i < points->size(); i++)
+	{
 		if (isInLesserSphere((*points)[i])) outside--;
 
 	}
